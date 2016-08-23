1820dab60eb5f0332e98e33997a5d55d84dd1f2a
roger.jiang <dkra4741@gmail.com>
2016-08-23 13:07:08 +0800

    add note Model, View, Routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
+'use strict'
 var express = require('express');
 var router = express.Router();
+var notes = require('../models/notes-memory')
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  notes.keylist()
+    .then(keylist => {
+      const keyPromises = []
+
+      for (let key of keylist) {
+        keyPromises.push(
+          notes.read(key)
+            .then(note => {
+              return {
+                key: note.key,
+                title: note.title,
+                body: note.body
+              }
+            })
+        );
+      }
+
+      return Promise.all(keyPromises)
+    })
+    .then(notelist => {
+      res.render('index', { title: 'Notes', notelist})
+    })
+    .catch(err => next(err))
 });
 
 module.exports = router;
